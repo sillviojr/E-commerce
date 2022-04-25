df821536afaa6ee8b8cac2a5a3c6e538152339f2
Davi Augusto <davi_augusto11@hotmail.com>
2022-04-25 20:11:33 -0300

    Rotas e listen adcionadas

--- program.js
+++ program.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 
-app.get(8080, ()=>{
+app.get("/", (req, res) =>{
+    res.send("Hello World!!!")
+})
+
+
+
+
+app.listen(8080, ()=>{
     console.log("Programa rodando!!")
 })
