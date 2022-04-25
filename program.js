e7a1ad605ee5dcd197d84643d6358a18ee6b659f
Davi Augusto <davi_augusto11@hotmail.com>
2022-04-25 20:37:09 -0300

    Rota cadastro criada

--- program.js
+++ program.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+//Rota principal
 app.get("/", (req, res) =>{
     res.send("Hello World!!!")
 })
 
+//Rota teste
+app.get("/cadastro", (req, res)=>{
+    res.render("home")
+})
 
 
 
