d8484da5ab20b44ecc629caa6b173d7f27947254
Leo-Morais <leo.vgvm@gmail.com>
2022-05-11 21:38:33 -0300

    adição de nova rota e conecção ao bd

--- program.js
+++ program.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const app = express()
+//const connection = require("/database/database")
+const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-
+/* 
+connection
+        .authenticate()
+        .then(()=>{
+            console.log("Conecção com banco de dados estabelecida")
+        }) .catch((error)=>{
+            console.log("error")
+        })
+*/
 //Rota principal
 app.get("/", (req, res) =>{
     res.render("home")
@@ -15,6 +25,10 @@ app.get("/cadastro", (req, res)=>{
     res.render("cadastro")
 })
 
+app.post("/salvarLogin", (req,res) =>{
+    let email = req.body.email
+    let senha = req.body.senha
+})
 
 
 app.listen(8080, ()=>{
