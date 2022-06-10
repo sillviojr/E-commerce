73b4995da06a78b02d8db3bfe7b1a51d2cd2aaeb
Davi Augusto <davi_augusto11@hotmail.com>
2022-06-10 13:55:13 -0300

    Importação de biblioteca: Express-session; Setando cookie e tempo de abertura da session; Criação de autenticação de login

--- program.js
+++ program.js
@@ -3,6 +3,7 @@ const app = express()
 const connection = require("./database/database")
 const bodyParser = require("body-parser")
 const bcrypt = require("bcryptjs")
+const session = require("express-session")
 const Usuario = require("./database/Usuario")
 const Cadastro = require("./database/Cadastro")
 const Products = require("./produtos/Products")
@@ -22,8 +23,19 @@ connection
             console.log(error)
         })
 
+app.use(session({
+    secret: "pkopapksokakoskapokapoasok", cookie: { maxAge: 30000 }
+}))
 
 app.use("/", productsController)
+
+app.get("/session", (req, res)=>{
+
+})
+
+app.get("/leitura", (req, res)=>{
+
+})
 
 //Rota principal
 app.get("/", (req, res) =>{
@@ -53,30 +65,51 @@ app.post("/salvarcadastro", (req,res) =>{
     let data = req.body.data
     let senha = req.body.senha
 
-    let salt = bcrypt.genSaltSync(10)
-    let hash = bcrypt.hashSync(senha, salt)
-    
-    Cadastro.create({
-        nome: nome,
-        email: email,
-        data: data,
-        senha: hash
-    }).then(()=>{
-            res.redirect("/")
-    }).catch((err)=>{
-        res.redirect("/")
+    Cadastro.findOne({where:{email: email}}).then(cadastros =>{
+        if(cadastros == undefined){
+
+            let salt = bcrypt.genSaltSync(10)
+            let hash = bcrypt.hashSync(senha, salt)
+            
+            Cadastro.create({
+                nome: nome,
+                email: email,
+                data: data,
+                senha: hash
+            }).then(()=>{
+                    res.redirect("/")
+            }).catch((err)=>{
+                res.redirect("/")
+            })
+        }else{
+            res.redirect("/salvarcadastro")
+        }
     })
+
+
 })
 
 app.post("/salvarlogin",(req,res) =>{
    let email = req.body.email
    let senha = req.body.senha
-   Usuario.create({
-        email: email, 
-        senha: senha
-   }).then(()=>{
-       res.redirect("/home")
-   }) 
+
+   Usuario.findOne({where:{email: email}}).then(usuarios =>{
+       if(usuarios != undefined){
+            let correct = bcrypt.compareSync(senha, usuarios.senha)
+
+            if(correct){
+                req.session.usuarios = {
+                    id: usuarios.id,
+                    email: usuarios.email
+                }
+                res.json(req.session.usuarios)
+            }else{
+                res.redirect("/salvarcadastro")
+            }
+       }else{
+           res.redirect("/salvarcadastro")
+       }
+   })
 })
 
 
